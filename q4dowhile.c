1eef427ddae2bd084997f2ffa71e7c50bf8fbe65
russellwang710017 <russell.wang@mail.mcgill.ca>
2015-07-19 14:38:31 -0400

    finishes q4dowhile.c

--- q4dowhile.c
+++ q4dowhile.c
@@ -2,23 +2,19 @@
 
 int main(void){
 	int input[100];
-	int i = 0;
+	int i = -1;
 	int sum=0;
 	float d=0.0;
 	float average;
 	
-	printf("Please enter a positive integer: ");
-	scanf("%d", &input[i]);
-	printf("The number you entered is %d.\n", input[i]);
-	
-	while(input[i] > 0){
+	do {
 		i++;
-		printf("Please enter a positive interger again: ");
+		printf("Please enter a positive integer: ");
 		scanf("%d", &input[i]);
 		printf("The number you entered is %d.\n", input[i]);
-	}
-	
-	d = i; //d is the total number of input positive numbers.
+	} while(input[i]>0);
+		
+	d=i;
 
 	for (i=0; i<d; i++){
 		sum+=input[i];
